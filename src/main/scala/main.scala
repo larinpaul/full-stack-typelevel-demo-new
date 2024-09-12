3f25931da6a2e48e1a490275df2e7e2b69190d0d
larinpaul <www.larinpavel@mail.ru>
2024-09-11 19:40:04 -0700

    Initial commit

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -11,4 +11,5 @@ def main(): Unit =
     //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
     // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
     println(s"i = $i")
+
   }
